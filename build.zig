3be7fa409922601372290ed54a8e89a84937a477
Marcus <6180175+MKindberg@users.noreply.github.com>
2024-06-13 22:36:41 +0200

    Bump to zig 0.13

--- build.zig
+++ build.zig
@@ -19,7 +19,7 @@ pub fn build(b: *std.Build) void {
         .name = "censor-ls",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -63,7 +63,7 @@ pub fn build(b: *std.Build) void {
     while (walker.next() catch unreachable) |entry| {
         if (!std.mem.endsWith(u8, entry.path, ".zig")) continue;
         const tests = b.addTest(.{
-            .root_source_file = .{ .path = b.fmt("src/{s}", .{entry.path}) },
+            .root_source_file = b.path(b.fmt("src/{s}", .{entry.path})),
             .target = target,
             .optimize = optimize,
         });
